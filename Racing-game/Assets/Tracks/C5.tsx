569ce6bf80404602396bbf8bd38a021256fa2d4b
Martí Mira Vázquez <160216136+algars15@users.noreply.github.com>
2024-12-18 14:15:22 +0100

    Track collisions c4 & c5

---
+++ Racing-game/Assets/Tracks/C5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="C5" tilewidth="1920" tileheight="1080" tilecount="1" columns="1">
+ <image source="C5.jpg" width="1920" height="1080"/>
+</tileset>
