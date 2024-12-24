b7a081843bdd989566f2274f74fc34c8c01a01c0
Martí Mira Vázquez <martiwp05@gmail.com>
2024-12-24 15:38:59 +0100

    working on tracks

--- Racing-game/Assets/Tracks/C4.tsx
+++ Racing-game/Assets/Tracks/C4.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="C4" tilewidth="1920" tileheight="1080" tilecount="1" columns="1">
- <image source="C4.jpg" width="1920" height="1080"/>
+<tileset version="1.10" tiledversion="1.11.0" name="C4" tilewidth="1920" tileheight="1080" tilecount="1" columns="1">
+ <image source="C4.png" width="1920" height="1080"/>
 </tileset>
